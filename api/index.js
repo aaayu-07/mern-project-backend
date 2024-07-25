644a0d78f11530f37cb63062df3fa26d07e53006
Ayush Raj <120264441+aaayu-07@users.noreply.github.com>
2024-07-25 17:36:41 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,44 +1,42 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
+const connectDB = require("../config/dbConnect");
+const corsOptions = require('../config/corsOptions');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-const connectDB = require("../config/dbConnect");
-const mongoose = require("mongoose");
-var cors = require('cors')
-const corsOptions = require('../config/corsOptions');
 
-
+// Connect to MongoDB
 connectDB();
-//user routes => /api/users and /api/user
-app.use(cors(corsOptions));
-app.use(express.json()); //middleware to parse json
 
-// user routes for /api/users and /api/user
+// Use CORS middleware with options
+app.use(cors(corsOptions));
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// User routes
 app.use("/api", require("../routes/userRoutes"));
 
-
-// article routes 
-
+// Article routes
 app.use("/api/articles", require("../routes/articleRoutes"));
 
-//tag routes
-
+// Tag routes
 app.use("/api/tags", require("../routes/tagRoutes"));
 
-//comment routes
-
+// Comment routes
 app.use("/api/articles", require("../routes/commentRoutes"));
 
-
+// MongoDB connection events
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
-
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 });
 
 mongoose.connection.on('error', (err) => {
-    console.log('Error while connection to MongoDB: ',err)
+  console.log('Error while connecting to MongoDB: ', err);
 });
