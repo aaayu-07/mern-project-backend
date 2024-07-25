b1966d0b3d434319d9b588b8c8c128b17887a2ac
Ayush Raj <120264441+aaayu-07@users.noreply.github.com>
2024-07-25 17:36:13 +0530

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,18 +1,20 @@
-const allowedOrigins = require('./allowedOrigins');
+// ../config/corsOptions.js
 
+const allowedOrigins = [
+  'https://mern-blogging-frontend.netlify.app', // Add other allowed origins if needed
+];
 
 const corsOptions = {
-    origin: (origin, callback) => {
-
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin ){
-            callback(null, true);
-        }else{
-            callback(new Error('Not allowed by CORS'));
-        }
-
-    },
-    credentials:true, //cookies, http authentication with cross origin requests
-    optionsSuccessStatus:200 //(CORS preflight checks)
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // if you need to handle cookies
+  optionsSuccessStatus: 204,
 };
 
 module.exports = corsOptions;
